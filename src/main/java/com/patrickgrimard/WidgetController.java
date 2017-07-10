bbff0d3a33413edd90723b91e556fb91b8ce3bf7
Federico <fede.calarco00@gmail.com>
2017-07-09 23:26:59 -0300

    Testing GitKraken

--- src/main/java/com/patrickgrimard/WidgetController.java
+++ src/main/java/com/patrickgrimard/WidgetController.java
@@ -14,6 +14,6 @@ public class WidgetController {
 
     @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public Widget index() {
-        return new Widget("green", 10, 7);
+        return new Widget("Hello git change.", 10, 7);
     }
 }
